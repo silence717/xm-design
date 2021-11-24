8dfd766665df1d84e6f15c9597f33c9e4341980f
fang.yang <yangfang2028@126.com>
2021-11-24 15:30:07 +0800

    ✨ [Button] Add size demo code

---
+++ components/Button/demo/size.tsx
@@ -0,0 +1,59 @@
+/**
+ * title.zh-CN: 加载中状态
+ * desc.zh-CN: 添加 loading 属性即可让按钮处于加载状态。
+ */
+
+import React from 'react';
+import { Button } from 'xm-design';
+
+export default function LoadingButton() {
+  return (
+    <section className="flex flex-col gap-2">
+      <div className="flex gap-2">
+        <Button type="primary" size="large">
+          Primary
+        </Button>
+        <Button size="large">Default</Button>
+        <Button type="dashed" size="large">
+          Dashed
+        </Button>
+        <Button type="danger" size="large">
+          Danger
+        </Button>
+        <Button type="link" size="large">
+          Dashed
+        </Button>
+      </div>
+      <div className="flex gap-2">
+        <Button type="primary" size="default">
+          Primary
+        </Button>
+        <Button size="default">Default</Button>
+        <Button type="dashed" size="default">
+          Dashed
+        </Button>
+        <Button type="danger" size="default">
+          Danger
+        </Button>
+        <Button type="link" size="default">
+          Dashed
+        </Button>
+      </div>
+      <div className="flex gap-2">
+        <Button type="primary" size="small">
+          Primary
+        </Button>
+        <Button size="small">Default</Button>
+        <Button type="dashed" size="small">
+          Dashed
+        </Button>
+        <Button type="danger" size="small">
+          Danger
+        </Button>
+        <Button type="link" size="small">
+          Dashed
+        </Button>
+      </div>
+    </section>
+  );
+}
--- components/Button/index.zh-CN.md
+++ components/Button/index.zh-CN.md
@@ -9,9 +9,7 @@ group:
   order: 1
 ---
 
-# Button
-
-按钮。
+# 按钮
 
 ## 代码演示
 
@@ -27,15 +25,15 @@ group:
 
 <code src="./demo/loading.tsx"></code>
 
+### 不同大小
+
+<code src="./demo/size.tsx"></code>
+
 ## API
 
 | 属性     | 说明                                                               | 类型            | 默认值    |
 | -------- | ------------------------------------------------------------------ | --------------- | --------- |
 | disabled | 按钮失效状态                                                       | boolean         | `false`   |
-| href     | 点击跳转的地址，设置此属性 `button` 的行为和 `a` 链接行为一致      | string          | -         |
-| target   | 设置 `a` 链接的 `target` 属性，`href` 存在时生效                   | string          | -         |
-| htmlType | 设置 `button` 原生属性 `type`                                      | string          | `button`  |
 | size     | 设置按钮大小，可选值为 `large` `default` `small` 或者不设          | string          | `default` |
 | type     | 设置按钮类型，可选值为 `primary` `dashed` `link` `normal` 或者不设 | string          | `normal`  |
-| block    | 将按钮宽度调整为其父宽度的选项                                     | boolean         | `false`   |
 | onClick  | 点击触发的回调                                                     | (event) => void | -         |
