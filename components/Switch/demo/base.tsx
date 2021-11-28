a1d0f8c519acf922bccf47fbb2dea8c5ac254772
fang.yang <yangfang2028@126.com>
2021-11-28 20:39:20 +0800

    📝 [Switch] demo code

--- components/Switch/demo/base.tsx
+++ components/Switch/demo/base.tsx
@@ -1,37 +1,47 @@
-import { Component } from 'react';
-import Switch from '../index';
+/**
+ * title.zh-CN: 基础用法
+ * desc.zh-CN: 在这个例子中， 按钮四种类型：主按钮、次按钮、虚线按钮、危险按钮、链接按钮。
+ */
+
+import React, { Component } from 'react';
+import { Switch } from 'xm-design';
 
 export default class ToggleDemo extends Component {
-    constructor(props: any) {
-        super(props);
+  constructor(props: any) {
+    super(props);
 
-        this.state = {
-            checked: true
-        };
-    }
-
-    handleChange = (checked: boolean) => {
-        this.setState({ checked });
+    this.state = {
+      checked: true,
     };
+  }
 
-    render() {
-        return (
-            <div>
-                <div>
-                    不添加checked状态：
-                    <Switch />
-                </div>
+  handleChange = (checked: boolean) => {
+    this.setState({ checked });
+  };
 
-                <div className="item">
-                    设置checked为true：
-                    <Switch checked={true} onChange={this.handleChange} />
-                </div>
+  render() {
+    return (
+      <div>
+        <div>
+          不添加checked状态：
+          <Switch />
+        </div>
 
-                <div>
-                    设置开关的文案显示：
-                    <Switch checked={false} checkedText="这里是开" unCheckedText="这里是关" onChange={this.handleChange} />
-                </div>
-            </div>
-        );
-    }
+        <div className="item">
+          设置checked为true：
+          <Switch checked={true} onChange={this.handleChange} />
+        </div>
+
+        <div>
+          设置开关的文案显示：
+          <Switch
+            checked={false}
+            checkedText="这里是开"
+            unCheckedText="这里是关"
+            onChange={this.handleChange}
+          />
+        </div>
+      </div>
+    );
+  }
 }
--- components/Switch/index.zh-CN.md
+++ components/Switch/index.zh-CN.md
@@ -9,14 +9,22 @@ group:
   order: 2
 ---
 
-# `Switch`
+# 开关
 
-> TODO: description
+## 代码演示
 
-## Usage
+### 基本使用
 
-```
-const switch = require('Switch');
+<code src="./demo/base.tsx"></code>
 
-// TODO: DEMONSTRATE API
-```
+### API
+
+| 属性           | 说明                                | 类型                                     | 默认值  |
+| -------------- | ----------------------------------- | ---------------------------------------- | ------- |
+| size           | 开关大小，可选值：`default` `small` | string                                   | default |
+| checked        | 指定当前是否选中                    | boolean                                  | false   |
+| checkedText    | 选中时候的文案                      | string                                   |         |
+| unCheckedText  | 未选中时候的文案                    | string                                   |         |
+| disabled       | 是否禁用                            | boolean                                  | false   |
+| onChange       | 切换状态时候的回调                  | Function(checked: boolean, event: Event) |         |
+| onBeforeChange | 切换状态前的二次确认                | Function()                               |         |
