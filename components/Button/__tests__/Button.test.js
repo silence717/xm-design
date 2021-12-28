8bc0ae373038e7a67cbd449b379b1bbc8246cccb
fang.yang <yangfang2028@126.com>
2021-12-28 22:42:11 +0800

    ➕ ✅ Add Button Component test demo

--- components/Button/__tests__/Button.test.js
+++ components/Button/__tests__/Button.test.js
@@ -3,8 +3,15 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Button from '../index'
 
-test('Button', () => {
-  const testButton = 'default'
-  render(<Button>{testButton}</Button>)
-  expect(screen.getByText(testButton)).toBeInTheDocument()
+describe('Button', () => {
+  it('should render same text passed into children prop', () => {
+    render(<Button>button</Button>)
+    expect(screen.getByText('button')).toBeInTheDocument()
+  })
+
+  it('should render same text passed into children prop', () => {
+    render(<Button>button</Button>)
+    const buttonElement = screen.getByRole('button')
+    expect(buttonElement).toBeInTheDocument()
+  })
 })
